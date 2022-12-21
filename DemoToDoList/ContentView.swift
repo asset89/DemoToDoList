3267e85f0ad7d6a9477d7f68db0cda9f2965cd7c
Asset Ryskul <asset89@gmail.com>
2022-12-21 14:54:59 +0600

    Initial Commit

---
+++ DemoToDoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoToDoList
+//
+//  Created by Asset Ryskul on 21.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoToDoList/DemoToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoToDoListApp.swift
+//  DemoToDoList
+//
+//  Created by Asset Ryskul on 21.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct DemoToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
